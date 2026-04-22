0accdc5497b32e7cc3879e0989202b7826867ff9
Johnson Wang <yuhang0229@gmail.com>
2026-04-22 16:11:55 +1000

    feat: add http communication between searchService and auctionService

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,12 +1,17 @@
+using System.Net;
 using MongoDB.Driver;
 using MongoDB.Entities;
+using Polly;
+using Polly.Extensions.Http;
 using SearchService.Data;
 using SearchService.Models;
+using SearchService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
 
 
 var app = builder.Build();
@@ -15,17 +20,26 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
-
-try
+app.Lifetime.ApplicationStarted.Register(async () =>
 {
-    await DbInitializer.InitDb(app);
+    try
+    {
+        await DbInitializer.InitDb(app);
 
-}
-catch (Exception ex)
-{
-    Console.WriteLine($"Error initializing database: {ex.Message}");
-}
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error initializing database: {ex.Message}");
+    }
+
+
+});
 
 
 app.Run();
 
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
