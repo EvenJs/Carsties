9ef1e7cfdb75bdf15ca09f43f764f72c1c117e2e
Johnson Wang <yuhang0229@gmail.com>
2026-04-28 10:38:05 +1000

    feat: create filter and sort

--- frontend/web-app/app/auctions/Listing.tsx
+++ frontend/web-app/app/auctions/Listing.tsx
@@ -6,19 +6,20 @@ import { getData } from "../actions/auctionActions";
 import { useEffect, useState } from "react";
 import { Auction, PagedResult } from "@/types";
 import Filter from "./Filter";
-import { useParams } from "next/navigation";
 import { useParamsStore } from "@/hooks/useParamsStore";
 import { useShallow } from "zustand/shallow";
 import qs from "query-string";
+import EmptyFilter from "../components/EmptyFilter";
 
 export default function Listing() {
-  // const data = await getData();
   const [data, setData] = useState<PagedResult<Auction>>();
   const params = useParamsStore(
     useShallow((state) => ({
       pageNumber: state.pageNumber,
       pageSize: state.pageSize,
       searchTerm: state.searchTerm,
+      orderBy: state.orderBy,
+      filterBy: state.filterBy,
     })),
   );
   const setParams = useParamsStore((state) => state.setParams);
@@ -42,19 +43,25 @@ export default function Listing() {
   return (
     <>
       <Filter />
-      <div className="grid grid-cols-4 gap-6">
-        {data &&
-          data.results.map((auction) => (
-            <AuctionCard key={auction.id} auction={auction} />
-          ))}
-      </div>
-      <div className="flex justify-center mt-4">
-        <AppPagination
-          pageChanged={setPageNumber}
-          currentPage={params.pageNumber}
-          pageCount={data.pageCount}
-        />
-      </div>
+      {data.totalCount == 0 ? (
+        <EmptyFilter />
+      ) : (
+        <>
+          <div className="grid grid-cols-4 gap-6">
+            {data &&
+              data.results.map((auction) => (
+                <AuctionCard key={auction.id} auction={auction} />
+              ))}
+          </div>
+          <div className="flex justify-center mt-4">
+            <AppPagination
+              pageChanged={setPageNumber}
+              currentPage={params.pageNumber}
+              pageCount={data.pageCount}
+            />
+          </div>
+        </>
+      )}
     </>
   );
 }
